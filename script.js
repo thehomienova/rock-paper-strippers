3c2020fa98339e26d7b26cf2303023ae6660f448
thehomienova <thehomienova@gmail.com>
2024-11-16 14:45:29 -0800

    play round

--- script.js
+++ script.js
@@ -6,27 +6,45 @@ function getComputerChoice () {
 
     switch (num) {
         case 1: choice = "rock";
-        console.log(choice);
         break;
 
         case 2: choice = "paper";
-        console.log(choice);
         break;
 
         case 3: choice = "strippers";
-        console.log(choice);
         break;
-
-
     }
+    console.log(choice);
+    return choice;
 }
 
 function getHumanChoice () {
-    let choice = prompt("Choose rock, paper, or strippers:")
-    
+    let choice = prompt("Choose rock, paper, or strippers:").toLowerCase();
     console.log(choice);
-
+    return choice;
 }
 
-let humanScore = 0;
 let computerScore = 0;
+let humanScore = 0;
+
+function playRound (computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        console.log ("its a tie");
+    } else if (computerChoice === "rock" && humanChoice === "strippers") {
+        console.log("computer wins!");
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        console.log("computer wins!");
+    } else if (computerChoice === "strippers" && humanChoice === "paper") {
+        console.log("computer wins!");
+    } else if (humanChoice === "rock" && computerChoice === "strippers") {
+        console.log("human wins!");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("human wins!");
+    } else if (humanChoice === "strippers" && computerChoice === "paper") {
+        console.log("human wins!");
+    }
+ 
+
+ }
+
+playRound(getComputerChoice(), getHumanChoice());
